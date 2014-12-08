2018b3cf3fa35c8086e42ee96fcc3f2095e0a198
XueFuxing <24309584@qq.com>
2014-12-08 10:49:49 +0800

    repair the BounceThread

--- src/BounceThread.java
+++ src/BounceThread.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,8 +12,13 @@ import javax.swing.JPanel;
 public class BounceThread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable(){
+			public void run(){
+				JFrame frame=new BounceFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
 	}
 
 }
